41b77e465259032f2953ff439c33c4e8764b6ca0
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2024-08-26 16:04:19 +0200

    tests: correct path for ci use

--- tests/test_interfere_base.py
+++ tests/test_interfere_base.py
@@ -1,10 +1,13 @@
+import pathlib
 import numpy as np
 
 import qpretrieve
 
+data_path = pathlib.Path(__file__).parent / "data"
+
 
 def test_interfere_base_best_interface():
-    edata = np.load("./data/hologram_cell.npz")
+    edata = np.load(data_path / "hologram_cell.npz")
 
     holo = qpretrieve.OffAxisHologram(data=edata["data"])
     assert holo.ff_iface.is_available

76e16ffbd52fc0d5e2a6fb3431e4ab29e16ec855
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2025-11-06 17:39:27 +0100

    feat: module for handling the ndarray backends numpy and cupy

---
+++ qpretrieve/_ndarray_backend.py
@@ -0,0 +1,54 @@
+import importlib
+
+
+_default_backend = "numpy"
+_xp = importlib.import_module(_default_backend)
+
+
+def get_ndarray_backend():
+    return _xp
+
+
+def set_ndarray_backend(backend_name: str = "numpy"):
+    """Return requested ndarray backend
+
+    Parameters
+    ----------
+    backend_name
+        Options are 'numpy' and 'cupy'
+
+    """
+    global _xp
+    try:
+        _xp = importlib.import_module(backend_name)
+        if backend_name == "cupy":
+            # cache should be cleared, as it contains numpy arrays
+            import qpretrieve
+            qpretrieve.filter.get_filter_array.cache_clear()
+    except ModuleNotFoundError as err:
+        raise ImportError(f"The backend '{backend_name}' is not installed. "
+                          f"Either install it or use the default backend: "
+                          f"{_default_backend}") from err
+
+def __getattr__(name):
+    """Expose this module as a proxy for numpy or cupy"""
+    return getattr(_xp, name)
+
+def _is_numpy():
+    return _xp.__name__.startswith("numpy")
+
+
+def _is_cupy():
+    return _xp.__name__.startswith("cupy")
+
+
+def _assert_is_numpy():
+    assert _is_numpy(), (
+        "ndarray_backend is not 'numpy', to use "
+        "'FFTFilterNumpy', run `qpretrieve.set_ndarray_backend('numpy')`.")
+
+
+def _assert_is_cupy():
+    assert _is_cupy(), (
+        "ndarray_backend is not 'cupy', to use "
+        "'FFTFilterCupy', run `qpretrieve.set_ndarray_backend('cupy')`.")

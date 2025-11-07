987eb26c57252b649870038e625fd403f9407cb4
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2025-11-07 14:54:16 +0100

    tests: add skip if cupy not installed

--- tests/test_ndarray_backend.py
+++ tests/test_ndarray_backend.py
@@ -1,6 +1,8 @@
+import pytest
+
 import qpretrieve
 
-import pytest
+from .helper_methods import skip_if_missing
 
 
 def test_ndarray_backend_numpy_default():
@@ -11,23 +13,6 @@ def test_ndarray_backend_numpy_default():
     assert qpretrieve._ndarray_backend._is_numpy()
 
 
-def test_ndarray_backend_cupy():
-    """should return cupy"""
-    assert qpretrieve._ndarray_backend._is_numpy()
-    qpretrieve.set_ndarray_backend('cupy')
-    assert qpretrieve._ndarray_backend._is_cupy()
-
-
-def test_ndarray_backend_swap():
-    """should return the correct set backend"""
-    qpretrieve.set_ndarray_backend('cupy')
-    assert qpretrieve._ndarray_backend._is_cupy()
-    qpretrieve.set_ndarray_backend('numpy')
-    assert qpretrieve._ndarray_backend._is_numpy()
-    qpretrieve.set_ndarray_backend('cupy')
-    assert qpretrieve._ndarray_backend._is_cupy()
-
-
 def test_ndarray_backend_bad():
     """should raise an ImportError"""
     bad_backend = "funpy"
@@ -36,3 +21,22 @@ def test_ndarray_backend_bad():
 
     with pytest.raises(ImportError, match=match_err_str):
         qpretrieve.set_ndarray_backend(bad_backend)
+
+
+@skip_if_missing("cupy")
+def test_ndarray_backend_cupy():
+    """should return cupy"""
+    assert qpretrieve._ndarray_backend._is_numpy()
+    qpretrieve.set_ndarray_backend('cupy')
+    assert qpretrieve._ndarray_backend._is_cupy()
+
+
+@skip_if_missing("cupy")
+def test_ndarray_backend_swap():
+    """should return the correct set backend"""
+    qpretrieve.set_ndarray_backend('cupy')
+    assert qpretrieve._ndarray_backend._is_cupy()
+    qpretrieve.set_ndarray_backend('numpy')
+    assert qpretrieve._ndarray_backend._is_numpy()
+    qpretrieve.set_ndarray_backend('cupy')
+    assert qpretrieve._ndarray_backend._is_cupy()
